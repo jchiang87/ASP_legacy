f3943fd851c1b8043ec964f4150cdb84cf7db941
jchiang <>
2006-07-18 16:14:39 +0000

    clean up example

--- pyASP/python/GBM_analysis.py
+++ pyASP/python/GBM_analysis.py
@@ -21,24 +21,18 @@ def notice(id):
 duration = 100
 ft1Merged = 'FT1_merged.fits'   # temporary file for FT1 data
 
-#gbmNotice = GbmNotice(notice('080104514'))
-#ft1, ft2 = getL1Data(gbmNotice.start_time - duration,
-#                     gbmNotice.start_time + duration)
-#ft1merge(ft1, ft1Merged)
-#gbmNotice = refinePosition(gbmNotice, ft1Input=ft1Merged,
-#                           extracted=False, tsmap=False, duration=duration)
-#like = LatGrbSpectrum(gbmNotice, ft1File=ft1Merged)
-
 import glob, sys
-files = glob.glob('/nfs/farm/g/glast/u33/jchiang/ASP/GRB/Notices/*NOTICE*')
+files = (notice('080104514'), )
+#files = glob.glob('/nfs/farm/g/glast/u33/jchiang/ASP/GRB/Notices/*NOTICE*')
 for i, item in enumerate(files):
     gbmNotice = GbmNotice(item)
-    ft1, ft2 = getL1Data(gbmNotice.start_time - duration,
-                         gbmNotice.start_time + duration)
     if gbmNotice.offAxisAngle() < 60 and not gbmNotice.inSAA():
         print "processing ", i, gbmNotice
         sys.stdout.flush()
         try:
+            ft1, ft2 = getL1Data(gbmNotice.start_time - duration,
+                                 gbmNotice.start_time + duration)
+            ft1merge(ft1, ft1Merged)
             gbmNotice = refinePosition(item, ft1Input=ft1Merged, tsmap=False,
                                        duration=duration)
             like = LatGrbSpectrum(gbmNotice, ft1File=ft1Merged)
