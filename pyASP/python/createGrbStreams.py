36dd465497ea1cad2e5386fc6596b28f9c6ffa1e
jchiang <>
2006-08-17 18:10:47 +0000

    refactor wrapperGenerator, createGrbStreams; remove unneeded development scripts

--- pyASP/python/createGrbStreams.py
+++ pyASP/python/createGrbStreams.py
@@ -11,7 +11,21 @@ import os
 import glob
 import time
 
-_pyASProot = '/nfs/farm/g/glast/u33/jchiang/ASP/pyASP/v0r1'
+def pyASProot():
+    version = os.environ['PYASPROOT'].split(os.path.sep)[-1]
+    return os.path.join('/nfs/farm/g/glast/u33/jchiang/ASP/pyASP', version)
+
+_pyASProot = pyASProot()
+_outputDir = os.environ['OUTPUTDIR']
+
+print "Using:\n\nPYASPROOT = %s\nOUTPUTDIR = %s\n" % (_pyASProot, _outputDir)
+
+def argString(argDict):
+    arg_string = (','.join(('output_dir=%s', 'PYASPROOT=%s'))
+               % (_outputDir, _pyASProot))
+    for item in argDict:
+        arg_string += ',%s=%s' % (item, argDict[item])
+    return arg_string    
 
 def streamNumber():
     time.sleep(1)     # to ensure unique stream numbers
@@ -27,16 +41,13 @@ class PipelineError(EnvironmentError):
     "Pipeline stream creation failed"
 
 def blindSearchStreams(downlinks=None):
-    os.chdir(os.environ['OUTPUTDIR'])
+    os.chdir(_outputDir)
     if downlinks is None:
         raise ValueError, "No downlink files specified"
     if isinstance(downlinks, str):
         downlinks = (downlinks, )
     for downlink in downlinks:
-        args = (','.join(('Downlink_file=%s',
-                          'output_dir=%s',
-                          'PYASPROOT=%s'))
-                % (downlink, os.environ['OUTPUTDIR'], _pyASProot))
+        args = argString({'Downlink_file': downlink})
         command = pipelineCommand('GRB_blind_search', args)
         print command
         rc = os.system(command)
@@ -44,17 +55,13 @@ def blindSearchStreams(downlinks=None):
             raise PipelineError, ("pipeline return code: %i" % rc)
 
 def refinementStreams(notices=None):
-    os.chdir(os.environ['OUTPUTDIR'])
+    os.chdir(_outputDir)
     if notices is None:
         notices = glob.glob('GRB*_Notice.txt')
     if isinstance(notices, str):
         notices = (notices, )
-
     for notice in notices:
-        args = (','.join(("GBM_Notice=%s",
-                          "output_dir=%s",
-                          "PYASPROOT=%s"))
-                % (notice, os.environ['OUTPUTDIR'], _pyASProot))
+        args = argString({'GBM_Notice': notice})
         command = pipelineCommand('GBM_refinement', args)
         print command
         rc = os.system(command)
@@ -62,24 +69,21 @@ def refinementStreams(notices=None):
             raise PipelineError, ("pipeline return code: %i" % rc)
 
 def afterglowStreams(parfiles=None):
-    os.chdir(os.environ['OUTPUTDIR'])
+    os.chdir(_outputDir)
     if parfiles is None:
         parfiles = glob.glob('GRB*_pars.txt')
     if isinstance(parfiles, str):
         parfiles = (parfiles, )
-
     for parfile in parfiles:
-        args = (','.join(("output_dir=%s",
-                          "GRB_parfile=%s",
-                          "PYASPROOT=%s"))
-                % (os.environ['OUTPUTDIR'], parfile, _pyASProot))
+        args = argString({'GRB_parfile': parfile})
         command = pipelineCommand('GRB_afterglow', args)
         print command
         rc = os.system(command)
         if rc != 0:
             raise PipelineError, ("pipeline return code: %i" % rc)
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
+    pass
 #    blindSearchStreams('foo')
 #    refinementStreams('bar')
 #    afterglowStreams('foobar')
