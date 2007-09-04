2a84ec73911b6d6ac4343bdacb107850dfe09ee3
jchiang <>
2007-09-04 20:43:26 +0000

    factor out common db functionality

--- grbASP/python/dbAccess.py
+++ grbASP/python/dbAccess.py
@@ -9,33 +9,12 @@
 import datetime
 import time
 import array
-import cx_Oracle
 import base64
-
-_connect_args = ("glastgen", "glast06", "GLASTP")
+from databaseAccess import apply, cx_Oracle
 
 def convert_clob(clob):
     foo = array.array('l', base64.decodestring(clob.read()))
     return foo
-
-def nullFunc(*args):
-    return None
-
-def apply(sql, cursorFunc=nullFunc):
-    my_connection = cx_Oracle.connect(*_connect_args)
-    cursor = my_connection.cursor()
-    try:
-        cursor.execute(sql)
-        results = cursorFunc(cursor)
-    except cx_Oracle.DatabaseError, message:
-        cursor.close()
-        my_connection.close()
-        raise cx_Oracle.DatabaseError, message
-    cursor.close()
-    if cursorFunc is nullFunc:
-        my_connection.commit()
-    my_connection.close()
-    return results
 
 def readGrb(grb_id):
     sql = "select * from GRB where GRB_ID = %i" % grb_id
