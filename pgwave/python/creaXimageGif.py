edced353242aaa87a2f5e5b9c8e37c33ddd04c7e
tosti <>
2007-11-26 22:38:11 +0000

    corrected a bug in generation of gif

--- pgwave/python/creaXimageGif.py
+++ pgwave/python/creaXimageGif.py
@@ -8,16 +8,17 @@ def creaXimageGif(nomefile):
 	f.close()
 	outgif1=nomefile.replace('.fits','.gif')
 	comando="ximage @temp.xco" 
-	comando1= ("convert %s -crop 575x285+180+165 %s\n" % (outgif,outgif1))
-	print comando1
+	#comando1= ("convert %s -crop 575x285+180+165 %s\n" % (outgif,outgif1))
+	comando1= ("convert %s %s\n" % (nomefile,outgif1))
+	#print comando1
 	try:
-		os.system(comando)
+		#os.system(comando)
 		os.system(comando1)
 		
 	except:
 		print "@error while trying to create gif"
 	
-	os.remove(outgif)	
+	#os.remove(outgif)	
 	
 if __name__ == '__main__':
 	creaXimageGif(sys.argv[1])
