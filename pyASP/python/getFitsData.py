f6a9387c81a1803ae475566aab7c3388f33b86d9
jchiang <>
2008-02-21 01:16:47 +0000

    find latest versions of files returned from data catalog query

--- pyASP/python/getFitsData.py
+++ pyASP/python/getFitsData.py
@@ -7,18 +7,35 @@ query of the datacatalog in a Pipeline II scriptlet.
 #
 # $Header$
 #
-import os, shutil
+import os, glob, shutil
+
+def filter_versions(fitsfiles):
+    """Filter the input file list so that only the most recent version 
+    of each dataset is returned."""
+    prefixes = {}
+    for item in fitsfiles:
+        tokens = item.split('_v')
+        dirname = os.path.dirname(tokens[0])
+        basename = os.path.basename(tokens[0])
+        prefixes[basename] = dirname, tokens[1]
+    outfiles = []
+    basenames = prefixes.keys()
+    # sort by basename (with version info sliced off) to get time ordering
+    basenames.sort()
+    for item in basenames:
+        fullpath = os.path.join(prefixes[item][0], 
+                                '_'.join((item, prefixes[item][1])))
+        outfiles.append(fullpath)
+    return outfiles
 
 def getFileList(filelist):
     output_dir = os.environ['OUTPUTDIR']
     shutil.copy(filelist, os.path.join(output_dir, filelist))
     fitsfiles = []
     for line in open(filelist):
-        # test if file exists
-        if os.path.isfile(line.strip().strip('+')):
+        if os.path.isfile(line.strip().strip('+')):    # file exists
             fitsfiles.append(line.strip().strip('+'))
-    fitsfiles.sort()   # assume this establishes time ordering
-    return fitsfiles
+    return filter_versions(fitsfiles)
 
 def getFitsData(ft1list='Ft1FileList', ft2list='Ft2FileList'):
     return getFileList(ft1list), getFileList(ft2list)
