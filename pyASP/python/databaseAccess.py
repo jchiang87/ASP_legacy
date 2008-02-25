4889cfabe66767a3712ab3d7caf16f0d2bb6f4d7
jchiang <>
2008-02-25 17:59:55 +0000

    refactor connection names; reset defaults

--- pyASP/python/databaseAccess.py
+++ pyASP/python/databaseAccess.py
@@ -11,13 +11,22 @@ import cx_Oracle
 db_config = open('/afs/slac/g/glast/ground/PipelineConfig/ASP/db_config', 'r')
 lines = db_config.readlines()
 
-glastp = lines[0].strip().encode('rot13').split()
-glastdev = lines[1].strip().encode('rot13').split()
+glastgen = lines[0].strip().encode('rot13').split()
+asp_prod = lines[1].strip().encode('rot13').split()
+asp_dev = lines[2].strip().encode('rot13').split()
+
+#
+# For backwards compatibility
+#
+glastp = glastgen
+glastdev = asp_dev
+
+asp_default = asp_dev
 
 def nullFunc(*args):
     return None
 
-def apply(sql, cursorFunc=nullFunc, connection=glastp):
+def apply(sql, cursorFunc=nullFunc, connection=glastgen):
     my_connection = cx_Oracle.connect(*connection)
     cursor = my_connection.cursor()
     try:
@@ -33,7 +42,7 @@ def apply(sql, cursorFunc=nullFunc, connection=glastp):
     my_connection.close()
     return results
 
-def getDbObjects(tablename, connection=glastp):
+def getDbObjects(tablename, connection=glastgen):
     """Return a list of entries for the specified db table"""
     sql = "SELECT * from %s" % tablename
     def cursorFunc(cursor):
