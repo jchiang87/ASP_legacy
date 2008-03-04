7e622377246618f61a6f76a07e16d4b083c7e2e5
jchiang <>
2008-03-04 00:56:39 +0000

    handle case where query returns empty file list

--- pyASP/python/getFitsData.py
+++ pyASP/python/getFitsData.py
@@ -13,8 +13,11 @@ from FileStager import FileStager
 def filter_versions(fitsfiles):
     """Filter the input file list so that only the most recent version 
     of each dataset is returned."""
-    if fitsfiles[0].find('_v') == -1:  # assume no versioning, so just return
-        return fitsfiles               # original list
+    try:
+        if fitsfiles[0].find('_v') == -1:  # assume no versioning, so just return
+            return fitsfiles               # original list
+    except IndexError:
+        return fitsfiles    # return empty file list
     prefixes = {}
     for item in fitsfiles:
         tokens = item.split('_v')
