86c36e1b262fb0c419f984b31f9e1023bd11c0a0
jchiang <>
2008-05-22 16:12:45 +0000

    give context for PIPELINESERVER message

--- pyASP/python/PipelineCommand.py
+++ pyASP/python/PipelineCommand.py
@@ -17,9 +17,9 @@ except KeyError:
     _pipelineServer = 'PROD'
     os.environ['PIPELINESERVER'] = _pipelineServer
 
-print "Using:"
+print "ASP/PipelineCommand, using:"
 print "PIPELINESERVER = %s" % _pipelineServer
-print ""
+print 
 
 def resolve_nfs_path(path):
     tokens = path.split(":")
