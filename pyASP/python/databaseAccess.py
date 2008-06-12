a069c3db50a710755e6ae42198a64a30c5eb8b88
jchiang <>
2008-06-12 21:00:37 +0000

    remove AspPolicy usage from req file; set default db connection strings based on pipeline context

--- pyASP/python/databaseAccess.py
+++ pyASP/python/databaseAccess.py
@@ -18,6 +18,7 @@ if os.environ['ORACLE_HOME'] == '/usr/oracle':
     glastgen = lines[0].strip().encode('rot13').split()
     asp_prod = lines[1].strip().encode('rot13').split()
     asp_dev = lines[2].strip().encode('rot13').split()
+    rsp_dev = lines[3].strip().encode('rot13').split()
 else:
     # 
     # Use Oracle wallet.
@@ -26,17 +27,36 @@ else:
     asp_prod = ('/@asp',)
     asp_dev = ('/@asp-dev',)
 
+#
+# Set the default default to point to the dev tables.
+#
 asp_default = asp_dev
-#asp_default = asp_prod
+
+try:
+    #
+    # Reset the default to prod if running in the PROD pipeline.
+    #
+    if os.environ['PIPELINESERVER'] == "PROD":
+        asp_default = asp_prod
+except KeyError:
+    #
+    # We may be running interactively (and did not set PIPELINESERVER), 
+    # so keep "DEV" as the default.
+    #
+    print "Warning: Using dev db tables."
+    pass
 
 def nullFunc(*args):
     return None
 
-def apply(sql, cursorFunc=nullFunc, connection=asp_default):
+def apply(sql, cursorFunc=nullFunc, connection=asp_default, args=None):
     my_connection = cx_Oracle.connect(*connection)
     cursor = my_connection.cursor()
     try:
-        cursor.execute(sql)
+        if args is None:
+            cursor.execute(sql)
+        else:
+            cursor.execute(sql, args)
         results = cursorFunc(cursor)
     except cx_Oracle.DatabaseError, message:
         cursor.close()
