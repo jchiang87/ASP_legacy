487c781c26c4e5c6b84e615e80a8ec095a514445
jchiang <>
2008-07-22 02:41:30 +0000

    set grb_id variable for scriptlet

--- grbASP/python/createAfterglowTarball.py
+++ grbASP/python/createAfterglowTarball.py
@@ -12,9 +12,17 @@ import os
 import pipeline
 from TarBall import TarBall
 from moveToXrootd import moveToXrootd
+import databaseAccess as dbAccess
 
 output_dir = os.environ['OUTPUTDIR']
 os.chdir(output_dir)
+
+pars = Parfile(os.environ['GRBPARS'])
+
+sql = "select GRB_ID from GRB where GCN_NAME = '%s'" % pars['name']
+grb_id = dbAccess.apply(sql, lambda curs : [x[0] for x in curs][0])
+print "grb_id = ", grb_id
+pipeline.setVariable('grb_id', grb_id)
 
 archive_name = "GRB_afterglow_%s.tar" % os.getcwd().split('/')[-1]
 
