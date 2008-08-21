3ee2440ad9f5091c36b13bb9e4d0410a7043a379
jchiang <>
2008-08-21 16:09:19 +0000

    use PGWAVE sourcesub_type for pgwave sources

--- drpMonitoring/python/drpDbAccess.py
+++ drpMonitoring/python/drpDbAccess.py
@@ -171,7 +171,8 @@ def findUniquePointSources(ra, dec, radius, roiIds=None, tol=0.5,
     ok_pgwave_sources = []
     cos_tol = num.cos(tol*num.pi/180.)
     for srcName in my_dict:
-        if my_dict[srcName].sourceType == 'Other_FSP':
+#        if my_dict[srcName].sourceType == 'Other_FSP':
+       if my_dict[srcName].sourceType == 'PGWAVE':
             addSource = True
             for knownSource in known_list:
                 nhat = num.array(my_dict[knownSource].nhat)
