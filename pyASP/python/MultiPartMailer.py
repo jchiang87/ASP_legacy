22885d970135f8fac387c00b95016e774729a022
jchiang <>
2009-06-29 23:32:02 +0000

    add reply_to option for MultiPartMailer

--- pyASP/python/MultiPartMailer.py
+++ pyASP/python/MultiPartMailer.py
@@ -16,10 +16,12 @@ import socket
 import time
 
 class MultiPartMailer(object):
-    def __init__(self, subject):
+    def __init__(self, subject, reply_to=None):
         self.message = StringIO.StringIO()
         self.writer = MimeWriter.MimeWriter(self.message)
         self.writer.addheader('Subject', subject)
+        if reply_to is not None:
+            self.writer.addheader('Reply-to', reply_to)
         self.writer.startmultipartbody('mixed')
     def add_text(self, text):
         part = self.writer.nextpart()
@@ -37,3 +39,9 @@ class MultiPartMailer(object):
         mailer = smtplib.SMTP('smtpunix.slac.stanford.edu')
         mailer.sendmail(sender, recipients, self.message.getvalue())
         mailer.close()
+
+if __name__ == '__main__':
+    print "sending test message"
+    mailer = MultiPartMailer('Test message', 'jchiang@slac.stanford.edu')
+    mailer.add_text("hello, world")
+    mailer.send("glastgcn@slac.stanford.edu", ('jchiang@slac.stanford.edu',))
