6e4e8871b80b5b613aae74c47ca187efce95524b
jrb <>
2009-08-28 22:13:12 +0000

    Changes/additions for SCons build

---
+++ asp_skymaps/asp_skymapsLib.py
@@ -0,0 +1,16 @@
+# @file asp_skymapsLib.py
+# @brief scons package dependencies for asp_skymaps
+#
+#$Header$
+def generate(env, **kw):
+	if not kw.get('depsOnly',0):
+		env.Tool('addLibrary', library=['skymaps'])
+        depends = 'facilities tip st_facilities asp_healpix embed_python'.split()
+        for pack in depends: env.Tool(pack+'Lib')
+
+	# why these?
+	#env.Tool('addLibrary', library=env['clhepLibs'])
+	#env.Tool('addLibrary', library=env['cfitsioLibs'])
+
+def exists(env):
+	return 1
