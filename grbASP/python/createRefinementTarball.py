254fd4d2d7b814eb9498a90e763f9b185f76731c
cvs2git <>
2009-10-23 02:42:26 +0000

    This commit was manufactured by cvs2svn to create tag 'grbASP-04-08-06'.

---
+++ grbASP/python/createRefinementTarball.py
@@ -0,0 +1,42 @@
+"""
+@brief Create a tarball of useful files used in the GRB_refinement task for 
+storage on xrootd.
+
+@author J. Chiang <jchiang@slac.stanford.edu>
+"""
+#
+# $Header$
+#
+
+import os
+import pipeline
+from TarBall import TarBall
+from moveToXrootd import moveToXrootd
+
+output_dir = os.environ['OUTPUTDIR']
+os.chdir(output_dir)
+
+archive_name = "GRB_refinement_%s.tar" % os.getcwd().split('/')[-1]
+
+targets = ('GRB*_BB_lc.dat',
+           'GRB*_LAT.fits',
+           'FT2_merged.fits',
+           'GRB*_Notice.txt',
+           'GRB*_tsmap.fits',
+           'GRB*_exp*.fits',
+           'GRB*_grb_spec.fits',
+           'GRB*_model.xml',
+           'GRB*_pars.txt',
+           'GRB*_findSrc.txt',
+           'countsMap_*.png',
+           'countsSpectra_*.png',
+           'errorContours_*.png',
+           'lightCurve_*.png')
+
+my_tarball = TarBall(archive_name)
+for target in targets:
+    my_tarball.append(target)
+my_tarball.gzip()
+
+outfile = moveToXrootd(archive_name + '.gz', output_dir)
+pipeline.setVariable('tarball_name', outfile)
