211b1100139531f0f2502ee455cf49b571bb8039
cvs2git <>
2010-09-09 15:44:13 +0000

    This commit was manufactured by cvs2svn to create tag 'AspLauncher-01-04-04'.

--- flareMonitoring/cmt/cleanup.sh
+++
@@ -1,9 +0,0 @@
-if test "${CMTROOT}" = ""; then
-  CMTROOT=/afs/slac.stanford.edu/g/glast/applications/CMT/v1r16p20040701; export CMTROOT
-fi
-. ${CMTROOT}/mgr/setup.sh
-tempfile=`${CMTROOT}/mgr/cmt build temporary_name -quiet`
-if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt -quiet cleanup -sh -pack=flareMonitoring -version=v0r1 -path=/a/surrey01/vol/vol2/g.svac/focke/ASP/code/newASP $* >${tempfile}; . ${tempfile}
-/bin/rm -f ${tempfile}
-
