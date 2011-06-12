be59ceb8210743f76561146b14c09e8880c41fc5
jchiang <>
2011-06-12 21:32:40 +0000

    update MultiPartMailer to use email module

--- pyASP/python/MultiPartMailer.py
+++ pyASP/python/MultiPartMailer.py
@@ -8,37 +8,32 @@ multiple image attachments.  Based on chapter 10.9 of Python Cookbook.
 # $Header$
 #
 
-import MimeWriter
-import base64
-import StringIO
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 import smtplib
-import socket
-import time
 
 class MultiPartMailer(object):
     def __init__(self, subject, reply_to=None):
-        self.message = StringIO.StringIO()
-        self.writer = MimeWriter.MimeWriter(self.message)
-        self.writer.addheader('Subject', subject)
+        self.message = MIMEMultipart()
+        self.message['Subject'] = subject
         if reply_to is not None:
-            self.writer.addheader('Reply-to', reply_to)
-        self.writer.startmultipartbody('mixed')
+            self.message['Reply-to'] = reply_to
     def add_text(self, text):
-        part = self.writer.nextpart()
-        body = part.startbody('text/plain')
-        body.write(text)
+        txt = MIMEText(text)
+        self.message.attach(txt)
     def add_image(self, image_file):
-        file_ext = image_file.split('.')[-1]
-        part = self.writer.nextpart()
-        part.addheader('Content-Transfer-Encoding', 'base64')
-        body = part.startbody('image/%s; name=%s' % (file_ext, image_file))
-        base64.encode(open(image_file, 'rb'), body)
-    def finish(self):
-        self.writer.lastpart()
+        fp = open(image_file, 'rb')
+        img = MIMEImage(fp.read())
+        fp.close()
+        self.message.attach(img)
     def send(self, sender, recipients):
         mailer = smtplib.SMTP('smtpunix.slac.stanford.edu')
-        mailer.sendmail(sender, recipients, self.message.getvalue())
+        mailer.sendmail(sender, recipients, self.message.as_string())
         mailer.close()
+    def finish(self):
+        pass
 
 if __name__ == '__main__':
     print "sending test message"
