a00b2e7dc74c7f89953be7db25e7d46b2a227eeb
cvs2git <>
2011-07-22 15:24:39 +0000

    This commit was manufactured by cvs2svn to create tag 'v5r0p0'.

--- asp_healpix/healpixLib.py
+++
@@ -1,8 +0,0 @@
-#$Id$
-def generate(env, **kw):
-    if not kw.get('depsOnly',0):
-        env.Tool('addLibrary', library = ['healpix'])
-    env.Tool('astroLib')
-    
-def exists(env):
-    return 1;
