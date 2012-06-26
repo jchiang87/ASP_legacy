32ba6cba52a5b2246d706901be961128eaf0a392
cvs2git <>
2012-06-26 20:05:07 +0000

    This commit was manufactured by cvs2svn to create tag 'grbASP-05-03-01'.

--- pyASP/pyASPLib.py
+++
@@ -1,8 +0,0 @@
-# $Header$
-def generate(env, **kw):
-    if not kw.get('depsOnly', 0):
-        env.Tool('addLibrary', library=['pyASP'])
-    env.Tool('astroLib')
-
-def exists(env):
-    return 1;
